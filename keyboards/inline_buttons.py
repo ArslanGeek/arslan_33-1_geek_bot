4e43fb7f40098fcfbd09703b93b9a15c087dfb94
ArslanGeek <bra.kreedz@gmail.com>
2023-09-16 19:58:35 +0600

    first commit

---
+++ keyboards/inline_buttons.py
@@ -0,0 +1,80 @@
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+async def start_keyboard():
+    makrup = InlineKeyboardMarkup()
+    questionnaire_button = InlineKeyboardButton(
+        "Начать опросник",
+        callback_data="start_questionnaire"
+    )
+    makrup.add(
+        questionnaire_button
+    )
+    return makrup
+
+async def question_first_keyboard():
+    makrup = InlineKeyboardMarkup()
+    male_button = InlineKeyboardButton(
+        "Male",
+        callback_data="male_answer"
+    )
+    female_button = InlineKeyboardButton(
+        "Female",
+        callback_data="female_answer"
+    )
+    makrup.add(
+        male_button
+    ).add(
+        female_button
+    )
+    return makrup
+
+# async def city_question_keyboard():
+#     makrup = InlineKeyboardMarkup()
+#     bishkek_button = InlineKeyboardButton(
+#         "Bishkek",
+#         callback_data="bishkek_answer"
+#     )
+#     osh_button = InlineKeyboardButton(
+#         "Osh",
+#         callback_data="osh_answer"
+#     )
+#     cholpon_ata_button = InlineKeyboardButton(
+#         "Cholpon Ata",
+#         callback_data="cholpon_ata_answer"
+#     )
+#     naryn_button = InlineKeyboardButton(
+#         "Naryn",
+#         callback_data="naryn_answer"
+#     )
+#     talas_button = InlineKeyboardButton(
+#         "Talas",
+#         callback_data="talas_answer"
+#     )
+#     batken_button = InlineKeyboardButton(
+#         "Batken",
+#         callback_data="batken_answer"
+#     )
+#     jalal_abad_button = InlineKeyboardButton(
+#         "Jalal Abad",
+#         callback_data="jalal_abad_answer"
+#     )
+#     makrup.add(
+#         bishkek_button
+#     ).add(
+#         osh_button
+#     ).add(
+#         cholpon_ata_button
+#     ).add(
+#         naryn_button
+#     ).add(
+#         batken_button
+#     ).add(
+#         talas_button
+#     ).add(
+#         jalal_abad_button
+#     )
+
+    # return makrup
