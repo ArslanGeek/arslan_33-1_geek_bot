cf4033a583689e78b972043778c8b954cc443a19
ArslanGeek <bra.kreedz@gmail.com>
2023-09-23 19:36:29 +0600

    third lesson

---
+++ const.py
@@ -0,0 +1,10 @@
+START_TEXT = "Hello, *{username}*!\n" \
+             "I am ArslanGeekBot, я здесь для того чтобы помочь тебе"
+
+
+
+PROFILE_CAPTION_TEXT = "Nickname: {nickname}\n" \
+                       "Bio: {bio}\n" \
+                       "Age: {age}\n" \
+                       "Occupation: {occupation}\n" \
+                       "Married: {married}\n"
